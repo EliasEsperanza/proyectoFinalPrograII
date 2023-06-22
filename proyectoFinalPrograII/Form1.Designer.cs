2c406d7c1882454784a666ec4864069f3d5b9d95
EliasEsperanza <eliasesperanza261380@gmail.com>
2023-06-22 15:03:34 -0600

    movi buttom

--- proyectoFinalPrograII/Form1.Designer.cs
+++ proyectoFinalPrograII/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(93, 139);
+            this.button1.Location = new System.Drawing.Point(114, 260);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(110, 52);
             this.button1.TabIndex = 0;
