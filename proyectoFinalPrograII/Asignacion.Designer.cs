897c4e94ce35c5437ab67b3f9f377103174c2e59
EliasEsperanza <eliasesperanza261380@gmail.com>
2023-06-23 12:34:43 -0600

    asignacion de variables

--- proyectoFinalPrograII/Asignacion.Designer.cs
+++ proyectoFinalPrograII/Asignacion.Designer.cs
@@ -28,12 +28,180 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.quest1 = new System.Windows.Forms.CheckBox();
+            this.quest2 = new System.Windows.Forms.CheckBox();
+            this.quest3 = new System.Windows.Forms.CheckBox();
+            this.quest4 = new System.Windows.Forms.CheckBox();
+            this.quest5 = new System.Windows.Forms.CheckBox();
+            this.quest6 = new System.Windows.Forms.CheckBox();
+            this.quest7 = new System.Windows.Forms.CheckBox();
+            this.quest8 = new System.Windows.Forms.CheckBox();
+            this.quest9 = new System.Windows.Forms.CheckBox();
+            this.quest10 = new System.Windows.Forms.CheckBox();
+            this.BTN_confirmar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(351, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Preguntas";
+            // 
+            // quest1
+            // 
+            this.quest1.AutoSize = true;
+            this.quest1.Location = new System.Drawing.Point(51, 96);
+            this.quest1.Name = "quest1";
+            this.quest1.Size = new System.Drawing.Size(247, 17);
+            this.quest1.TabIndex = 1;
+            this.quest1.Text = "¿Cómo describirías tu capacidad de liderazgo?";
+            this.quest1.UseVisualStyleBackColor = true;
+            // 
+            // quest2
+            // 
+            this.quest2.AutoSize = true;
+            this.quest2.Location = new System.Drawing.Point(51, 153);
+            this.quest2.Name = "quest2";
+            this.quest2.Size = new System.Drawing.Size(232, 17);
+            this.quest2.TabIndex = 2;
+            this.quest2.Text = "¿Cómo manejas los conflictos en el trabajo?";
+            this.quest2.UseVisualStyleBackColor = true;
+            // 
+            // quest3
+            // 
+            this.quest3.AutoSize = true;
+            this.quest3.Location = new System.Drawing.Point(51, 209);
+            this.quest3.Name = "quest3";
+            this.quest3.Size = new System.Drawing.Size(312, 17);
+            this.quest3.TabIndex = 3;
+            this.quest3.Text = "¿Cómo te mantienes actualizado/a en tu campo profesional?";
+            this.quest3.UseVisualStyleBackColor = true;
+            // 
+            // quest4
+            // 
+            this.quest4.AutoSize = true;
+            this.quest4.Location = new System.Drawing.Point(51, 265);
+            this.quest4.Name = "quest4";
+            this.quest4.Size = new System.Drawing.Size(331, 17);
+            this.quest4.TabIndex = 4;
+            this.quest4.Text = "¿Cómo aseguras el cumplimiento de los plazos en tus proyectos?";
+            this.quest4.UseVisualStyleBackColor = true;
+            // 
+            // quest5
+            // 
+            this.quest5.AutoSize = true;
+            this.quest5.Location = new System.Drawing.Point(51, 318);
+            this.quest5.Name = "quest5";
+            this.quest5.Size = new System.Drawing.Size(229, 17);
+            this.quest5.TabIndex = 5;
+            this.quest5.Text = "¿Cómo te adaptas a cambios inesperados?";
+            this.quest5.UseVisualStyleBackColor = true;
+            // 
+            // quest6
+            // 
+            this.quest6.AutoSize = true;
+            this.quest6.Location = new System.Drawing.Point(455, 96);
+            this.quest6.Name = "quest6";
+            this.quest6.Size = new System.Drawing.Size(298, 17);
+            this.quest6.TabIndex = 6;
+            this.quest6.Text = "¿Cuál es tu mayor logro profesional y cómo lo alcanzaste?";
+            this.quest6.UseVisualStyleBackColor = true;
+            // 
+            // quest7
+            // 
+            this.quest7.AutoSize = true;
+            this.quest7.Location = new System.Drawing.Point(455, 153);
+            this.quest7.Name = "quest7";
+            this.quest7.Size = new System.Drawing.Size(235, 17);
+            this.quest7.TabIndex = 7;
+            this.quest7.Text = "¿Cómo manejas situaciones de alta presión?";
+            this.quest7.UseVisualStyleBackColor = true;
+            // 
+            // quest8
+            // 
+            this.quest8.AutoSize = true;
+            this.quest8.Location = new System.Drawing.Point(455, 209);
+            this.quest8.Name = "quest8";
+            this.quest8.Size = new System.Drawing.Size(333, 17);
+            this.quest8.TabIndex = 8;
+            this.quest8.Text = "¿Qué haces para fomentar la colaboración en tu entorno laboral?";
+            this.quest8.UseVisualStyleBackColor = true;
+            // 
+            // quest9
+            // 
+            this.quest9.AutoSize = true;
+            this.quest9.Location = new System.Drawing.Point(455, 265);
+            this.quest9.Name = "quest9";
+            this.quest9.Size = new System.Drawing.Size(207, 17);
+            this.quest9.TabIndex = 9;
+            this.quest9.Text = "¿Cómo abordas problemas complejos?";
+            this.quest9.UseVisualStyleBackColor = true;
+            // 
+            // quest10
+            // 
+            this.quest10.AutoSize = true;
+            this.quest10.Location = new System.Drawing.Point(455, 318);
+            this.quest10.Name = "quest10";
+            this.quest10.Size = new System.Drawing.Size(275, 17);
+            this.quest10.TabIndex = 10;
+            this.quest10.Text = "¿Cómo te mantienes motivado/a en tu trabajo diario?";
+            this.quest10.UseVisualStyleBackColor = true;
+            // 
+            // BTN_confirmar
+            // 
+            this.BTN_confirmar.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BTN_confirmar.Image = global::proyectoFinalPrograII.Properties.Resources.icons8_confirmar;
+            this.BTN_confirmar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BTN_confirmar.Location = new System.Drawing.Point(275, 366);
+            this.BTN_confirmar.Name = "BTN_confirmar";
+            this.BTN_confirmar.Size = new System.Drawing.Size(242, 52);
+            this.BTN_confirmar.TabIndex = 11;
+            this.BTN_confirmar.Text = "Confirmar";
+            this.BTN_confirmar.UseVisualStyleBackColor = false;
+            // 
+            // Asignacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Asignacion";
+            this.Controls.Add(this.BTN_confirmar);
+            this.Controls.Add(this.quest10);
+            this.Controls.Add(this.quest9);
+            this.Controls.Add(this.quest8);
+            this.Controls.Add(this.quest7);
+            this.Controls.Add(this.quest6);
+            this.Controls.Add(this.quest5);
+            this.Controls.Add(this.quest4);
+            this.Controls.Add(this.quest3);
+            this.Controls.Add(this.quest2);
+            this.Controls.Add(this.quest1);
+            this.Controls.Add(this.label1);
+            this.Name = "Asignacion";
+            this.Text = "Asignacion de Preguntas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox quest1;
+        private System.Windows.Forms.CheckBox quest2;
+        private System.Windows.Forms.CheckBox quest3;
+        private System.Windows.Forms.CheckBox quest4;
+        private System.Windows.Forms.CheckBox quest5;
+        private System.Windows.Forms.CheckBox quest6;
+        private System.Windows.Forms.CheckBox quest7;
+        private System.Windows.Forms.CheckBox quest8;
+        private System.Windows.Forms.CheckBox quest9;
+        private System.Windows.Forms.CheckBox quest10;
+        private System.Windows.Forms.Button BTN_confirmar;
     }
 }
