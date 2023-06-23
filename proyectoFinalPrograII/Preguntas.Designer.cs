8e47d835f10d369a28182b3752028e8fd77ad714
EliasEsperanza <eliasesperanza261380@gmail.com>
2023-06-23 14:37:05 -0600

    Asignacion de variables 2

--- proyectoFinalPrograII/Preguntas.Designer.cs
+++ proyectoFinalPrograII/Preguntas.Designer.cs
@@ -28,12 +28,241 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.score1 = new System.Windows.Forms.NumericUpDown();
+            this.answer1 = new System.Windows.Forms.Label();
+            this.answer2 = new System.Windows.Forms.Label();
+            this.score2 = new System.Windows.Forms.NumericUpDown();
+            this.answer3 = new System.Windows.Forms.Label();
+            this.score3 = new System.Windows.Forms.NumericUpDown();
+            this.answer4 = new System.Windows.Forms.Label();
+            this.score4 = new System.Windows.Forms.NumericUpDown();
+            this.answer5 = new System.Windows.Forms.Label();
+            this.score5 = new System.Windows.Forms.NumericUpDown();
+            this.BTN_guardarResultado = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.score1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.score2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.score3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.score4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.score5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // score1
+            // 
+            this.score1.Location = new System.Drawing.Point(359, 80);
+            this.score1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.score1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.score1.Name = "score1";
+            this.score1.Size = new System.Drawing.Size(46, 20);
+            this.score1.TabIndex = 1;
+            this.score1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // answer1
+            // 
+            this.answer1.AutoSize = true;
+            this.answer1.Location = new System.Drawing.Point(70, 82);
+            this.answer1.Name = "answer1";
+            this.answer1.Size = new System.Drawing.Size(59, 13);
+            this.answer1.TabIndex = 2;
+            this.answer1.Text = "Pregunta 1";
+            // 
+            // answer2
+            // 
+            this.answer2.AutoSize = true;
+            this.answer2.Location = new System.Drawing.Point(70, 140);
+            this.answer2.Name = "answer2";
+            this.answer2.Size = new System.Drawing.Size(59, 13);
+            this.answer2.TabIndex = 3;
+            this.answer2.Text = "Pregunta 2";
+            // 
+            // score2
+            // 
+            this.score2.Location = new System.Drawing.Point(359, 133);
+            this.score2.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.score2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.score2.Name = "score2";
+            this.score2.Size = new System.Drawing.Size(46, 20);
+            this.score2.TabIndex = 4;
+            this.score2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // answer3
+            // 
+            this.answer3.AutoSize = true;
+            this.answer3.Location = new System.Drawing.Point(70, 198);
+            this.answer3.Name = "answer3";
+            this.answer3.Size = new System.Drawing.Size(59, 13);
+            this.answer3.TabIndex = 5;
+            this.answer3.Text = "Pregunta 3";
+            // 
+            // score3
+            // 
+            this.score3.Location = new System.Drawing.Point(359, 191);
+            this.score3.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.score3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.score3.Name = "score3";
+            this.score3.Size = new System.Drawing.Size(46, 20);
+            this.score3.TabIndex = 6;
+            this.score3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // answer4
+            // 
+            this.answer4.AutoSize = true;
+            this.answer4.Location = new System.Drawing.Point(70, 254);
+            this.answer4.Name = "answer4";
+            this.answer4.Size = new System.Drawing.Size(59, 13);
+            this.answer4.TabIndex = 7;
+            this.answer4.Text = "Pregunta 4";
+            // 
+            // score4
+            // 
+            this.score4.Location = new System.Drawing.Point(359, 247);
+            this.score4.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.score4.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.score4.Name = "score4";
+            this.score4.Size = new System.Drawing.Size(46, 20);
+            this.score4.TabIndex = 8;
+            this.score4.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // answer5
+            // 
+            this.answer5.AutoSize = true;
+            this.answer5.Location = new System.Drawing.Point(70, 304);
+            this.answer5.Name = "answer5";
+            this.answer5.Size = new System.Drawing.Size(59, 13);
+            this.answer5.TabIndex = 9;
+            this.answer5.Text = "Pregunta 5";
+            // 
+            // score5
+            // 
+            this.score5.Location = new System.Drawing.Point(359, 302);
+            this.score5.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.score5.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.score5.Name = "score5";
+            this.score5.Size = new System.Drawing.Size(46, 20);
+            this.score5.TabIndex = 10;
+            this.score5.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // BTN_guardarResultado
+            // 
+            this.BTN_guardarResultado.Location = new System.Drawing.Point(142, 368);
+            this.BTN_guardarResultado.Name = "BTN_guardarResultado";
+            this.BTN_guardarResultado.Size = new System.Drawing.Size(182, 34);
+            this.BTN_guardarResultado.TabIndex = 11;
+            this.BTN_guardarResultado.Text = "Guardar resultado";
+            this.BTN_guardarResultado.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(197, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Calificar";
+            // 
+            // Preguntas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(501, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.BTN_guardarResultado);
+            this.Controls.Add(this.score5);
+            this.Controls.Add(this.answer5);
+            this.Controls.Add(this.score4);
+            this.Controls.Add(this.answer4);
+            this.Controls.Add(this.score3);
+            this.Controls.Add(this.answer3);
+            this.Controls.Add(this.score2);
+            this.Controls.Add(this.answer2);
+            this.Controls.Add(this.answer1);
+            this.Controls.Add(this.score1);
+            this.Name = "Preguntas";
             this.Text = "Preguntas";
+            ((System.ComponentModel.ISupportInitialize)(this.score1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.score2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.score3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.score4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.score5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown score1;
+        private System.Windows.Forms.Label answer1;
+        private System.Windows.Forms.Label answer2;
+        private System.Windows.Forms.NumericUpDown score2;
+        private System.Windows.Forms.Label answer3;
+        private System.Windows.Forms.NumericUpDown score3;
+        private System.Windows.Forms.Label answer4;
+        private System.Windows.Forms.NumericUpDown score4;
+        private System.Windows.Forms.Label answer5;
+        private System.Windows.Forms.NumericUpDown score5;
+        private System.Windows.Forms.Button BTN_guardarResultado;
+        private System.Windows.Forms.Label label6;
     }
 }
