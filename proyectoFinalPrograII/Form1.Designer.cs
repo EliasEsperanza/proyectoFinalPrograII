781a68b4c50dc9459ae7b428949c1d8dd5b48a09
EliasEsperanza <eliasesperanza261380@gmail.com>
2023-06-23 11:21:34 -0600

    Diseño del LOGIN

--- proyectoFinalPrograII/Form1.Designer.cs
+++ proyectoFinalPrograII/Form1.Designer.cs
@@ -28,37 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BTN_Login = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TB_User = new System.Windows.Forms.TextBox();
+            this.TB_passWord = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(107, 262);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 52);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Iniciar Sesion";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 37);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 72);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(143, 13);
+            this.label1.Size = new System.Drawing.Size(65, 16);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Nombre(Elias se la come xd):";
+            this.label1.Text = "Usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(59, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contraseña:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::proyectoFinalPrograII.Properties.Resources.icons8_password;
+            this.pictureBox2.Location = new System.Drawing.Point(23, 146);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::proyectoFinalPrograII.Properties.Resources.icons8_login_50;
+            this.pictureBox1.Location = new System.Drawing.Point(23, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BTN_Login
+            // 
+            this.BTN_Login.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BTN_Login.Image = global::proyectoFinalPrograII.Properties.Resources.icons8_login;
+            this.BTN_Login.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BTN_Login.Location = new System.Drawing.Point(62, 262);
+            this.BTN_Login.Name = "BTN_Login";
+            this.BTN_Login.Size = new System.Drawing.Size(185, 37);
+            this.BTN_Login.TabIndex = 0;
+            this.BTN_Login.Text = "Iniciar Sesion";
+            this.BTN_Login.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(92, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Bienvenido";
+            // 
+            // TB_User
+            // 
+            this.TB_User.Location = new System.Drawing.Point(168, 65);
+            this.TB_User.Name = "TB_User";
+            this.TB_User.Size = new System.Drawing.Size(133, 20);
+            this.TB_User.TabIndex = 6;
+            // 
+            // TB_passWord
+            // 
+            this.TB_passWord.Location = new System.Drawing.Point(168, 155);
+            this.TB_passWord.Name = "TB_passWord";
+            this.TB_passWord.Size = new System.Drawing.Size(133, 20);
+            this.TB_passWord.TabIndex = 7;
             // 
             // LOGIN
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(325, 430);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(325, 356);
+            this.Controls.Add(this.TB_passWord);
+            this.Controls.Add(this.TB_User);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BTN_Login);
             this.Name = "LOGIN";
             this.Text = "LOGIN";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,8 +141,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BTN_Login;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TB_User;
+        private System.Windows.Forms.TextBox TB_passWord;
     }
 }
 
