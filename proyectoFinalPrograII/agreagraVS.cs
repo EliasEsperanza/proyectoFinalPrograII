51d8a4c51f11e3a2cdf071f4ab05737b54785849
AngelRamirez18 <angelosmin.10@gmail.com>
2023-06-25 17:14:04 -0600

    jjajajja

--- proyectoFinalPrograII/agreagraVS.cs
+++ proyectoFinalPrograII/agreagraVS.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace proyectoFinalPrograII
 {
-    internal class agreagraVS
+    public class agreagraVS
     {
+        public string NombreV;
+        public string DescripcionV;
+        public string RequisitosV;
+        public double SalarioV;
+        public string FCDEIV;
+        public string FCDEFinV;
+        public int CantidadV;
+        public int T;
+
+        public agreagraVS(string nombre, string descripcion, string requisito, double salario, string FCDEIV,
+                          string FCDEFinV, int cantidad, int t)
+        {
+            this.NombreV = nombre;
+            this.DescripcionV = descripcion;
+            this.RequisitosV = requisito;
+            this.SalarioV = salario;
+            this.FCDEIV = FCDEIV;
+            this.FCDEFinV = FCDEFinV;
+            this.CantidadV = cantidad;
+            T = t;
+        }
     }
 }
